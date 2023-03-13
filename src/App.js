00b2071f2c3e4f893c93b8f3228fce4d80bdc0c8
Helcio <helcioanicio19@gmail.com>
2023-03-13 19:57:36 -0300

    Changed the origin files, app.js and index.js and changed to scss, and added a folder to my styles scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.scss';
+// import './Components/Header/Header'
+import Home from './Components/Home/Home';
+import Menu from './Components/Menu/Menu';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Technology from './Components/Technology/Technology';
+import Footer from './Components/Footer/Footer';
+// import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu/>
+      <Home/>
+      <Technology/>
+      <Footer/>
+
+
+      <Portfolio/>
+    </>
   );
 }
 
