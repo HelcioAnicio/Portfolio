13a1b048395610bb8eb78774f2e6d171b9cf0b71
Helcio <helcioanicio19@gmail.com>
2023-03-14 22:20:19 -0300

    Coded the Home and styled too

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,18 @@ import './Home.modules.scss'
 function Home () {
     return (
         <>
-            <h6>Home</h6>
+            <div className="ContainerText">
+                <h1>Helcio Anicio</h1>
+                <p>
+                Hello,<span>I'm a Front-end developer</span>, I work as a freelancer creating web pages. <br />
+                    I have knowledge in several technologies, and several projects that show a little of my work.
+                    If you are in need of a quality, professional and updated page, contact me using the button below.
+                </p>
+                <button>Make your budget!</button>
+            </div>
+            <div className="CellPhone">
+
+            </div>
         </>
     )
 }
