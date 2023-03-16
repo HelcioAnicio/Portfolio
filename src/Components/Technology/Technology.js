701c1564aefcc6245053cc689f27e796c2edfdab
Helcio <helcioanicio19@gmail.com>
2023-03-15 23:15:07 -0300

    Added the component Technology on App.js, I coded the component and styled it

--- src/Components/Technology/Technology.js
+++ src/Components/Technology/Technology.js
@@ -1,11 +1,67 @@
-import './Technology.modules.scss'
+import './Technology.modules.scss';
+import { FaReact } from 'react-icons/fa';
+import { TbBrandJavascript } from "react-icons/tb";
+import { FaHtml5 } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+
 
 function Technology () {
     return (
         <>
-        <h6>Technology</h6>
-        </>
+            <section className='SectionTech'>
+                <div className='DivContainer'>
+                    <div className="React">
+                        <div>
+                            <FaReact/>
+                        </div>
+                        <div>
+                            <p>React.Js</p>
+                        </div>
+                    </div>
+                    <div className="Javascript">
+                        <div>
+                            <TbBrandJavascript/>
+                        </div>
+                        <div>
+                            <p>JavaScript</p>
+                        </div>
+                    </div>
+                    <div className="Scss">
+                        <div>
+                            <FaSass/>
+                        </div>
+                        <div>
+                            <p>Scss</p>
+                        </div>
+                    </div>
+                    <div className="Html">
+                        <div>
+                            <FaHtml5/>
+                        </div>
+                        <div>
+                            <p>Html5</p>
+                        </div>
+                    </div>
+                    <div className="Css3">
+                        <div>
+
+                        </div>
+                        <div>
+                            <p>Css3</p>
+                        </div>
+                    </div>
+                    <div className="Firebase">
+                        <div>
+
+                        </div>
+                        <div>
+                            <p>Firebase</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+    </>
     )
 }
-
 export default Technology;
