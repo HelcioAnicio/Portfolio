59453f96cbaa89ae1eccef1578b1142a0af3a327
Helcio <helcioanicio19@gmail.com>
2023-03-18 23:26:42 -0300

    Change some properties on Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,49 +1,54 @@
 import './Header.modules.scss';
 import { GiHamburgerMenu } from "react-icons/gi";
-
-const Menu = document.getElementsByClassName('Menu')
-
-function ToggleMenu() {
-    const Nav = document.getElementById('ContainerHeader')
-    Nav.classList.toggle('active');
-}
-
-Menu.addEventListener('click',ToggleMenu)
-
+import { TfiClose } from "react-icons/tfi";
+import { useState, useEffect} from 'react';
 
 function Header () {
+	const [isOpen, setIsOpen] = useState(false)
+	
+	useEffect(() => {
+		isOpen ? document.body.style.overflowY = 'hidden' :  document.body.style.overflowY = 'scroll'
+	}, [isOpen])
     return (
         <>
             <nav id='ContainerHeader'>
                 <div className='DivMenu'>
-                    <GiHamburgerMenu className='Menu'/>
+                    <button onClick={() => setIsOpen(true)}>
+                        <GiHamburgerMenu className='Menu'/>
+                    </button>
                 </div>
                 <ul id='DivPages'>
                     <li><a href="https://google.com">Home</a></li>
                     <li><a href="https://google.com">About Me</a></li>
-                    <li><a href="https://google.com">Technology</a></li>
+                    <li><a href="#Tech">Technology</a></li>
                     <li><a href="https://google.com">Portfolio</a></li>
                     <li><a href="https://google.com">Talk to me</a></li> 
                 </ul>
             </nav>
-
-            {/* <nav className='MobileMenu'>
-                <ul className='Menu'>
-                    <div>
-                        <GiHamburgerMenu/>
-                    </div>
-                    <li><a href="https://google.com">Home</a></li>
-                    <li><a href="https://google.com">About Me</a></li>
-                    <li><a href="https://google.com">Technology</a></li>
-                    <li><a href="https://google.com">Portfolio</a></li>
-                    <li><a href="https://google.com">Talk to me</a></li> 
-
-                </ul>
-            </nav> */}
+			
+            {isOpen && <MenuMobile setIsOpen={setIsOpen} />}
 
         </>
     )
-
 }
-
 export default Header;
+
+
+function MenuMobile({setIsOpen}) {
+    return (
+    <nav className='MobileMenu'>
+        <ul className='Menu'>
+            <div>
+                <button className='ButtonClose' onClick={() => setIsOpen(false)}>
+                    <TfiClose/>
+                </button>
+            </div>
+            <li><a href="https://google.com">Home</a></li>
+            <li><a href="https://google.com">About Me</a></li>
+            <li><a href="#Tech">Technology</a></li>
+            <li><a href="https://google.com">Portfolio</a></li>
+            <li><a href="https://google.com">Talk to me</a></li> 
+        </ul>
+    </nav> 
+    )
+}
