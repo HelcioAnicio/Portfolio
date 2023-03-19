1721af7aa8a9c28614c5ddd738150bcbc26780ac
Helcio <helcioanicio19@gmail.com>
2023-03-18 23:27:23 -0300

    Change some properties on technology

--- src/Components/Technology/Technology.js
+++ src/Components/Technology/Technology.js
@@ -1,62 +1,51 @@
 import './Technology.modules.scss';
-import { FaReact } from 'react-icons/fa';
+import { FaReact, FaHtml5, FaSass } from 'react-icons/fa';
 import { TbBrandJavascript } from "react-icons/tb";
-import { FaHtml5 } from "react-icons/fa";
-import { FaSass } from "react-icons/fa";
+import { IoLogoFirebase, IoLogoNodejs } from 'react-icons/io5'
+// import { IoLogoCss3 } from 'react-icons/io5'
 
 
 function Technology () {
     return (
         <>
-            <section className='SectionTech'>
+            <section id='Tech' className='SectionTech'>
+                <h2>Tecnologias</h2>
                 <div className='DivContainer'>
                     <div className="React">
                         <div>
                             <FaReact/>
                         </div>
-                        <div>
-                            <p>React.Js</p>
-                        </div>
+                        <p>React.Js</p>
                     </div>
                     <div className="Javascript">
                         <div>
                             <TbBrandJavascript/>
                         </div>
-                        <div>
-                            <p>JavaScript</p>
-                        </div>
+                        <p>JavaScript</p>
                     </div>
                     <div className="Scss">
                         <div>
                             <FaSass/>
                         </div>
-                        <div>
-                            <p>Scss</p>
-                        </div>
+                        <p>Scss</p>
                     </div>
                     <div className="Html">
                         <div>
                             <FaHtml5/>
                         </div>
-                        <div>
-                            <p>Html5</p>
-                        </div>
+                        <p>Html5</p>
                     </div>
-                    <div className="Css">
+                    <div className="Node">
                         <div>
-                            <FaHtml5/>
+                            <IoLogoNodejs/>
                         </div>
-                        <div>
-                            <p>Css3</p>
-                        </div>
+                        <p>Node.Js</p>
                     </div>
                     <div className="Firebase">
                         <div>
-                            <FaHtml5/>
+                            <IoLogoFirebase/>
                         </div>
-                        <div>
-                            <p>Firebase</p>
-                        </div>
+                        <p>Firebase</p>
                     </div>
 
                 </div>
