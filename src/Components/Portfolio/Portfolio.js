59faa396a8c28d9c9b2c359b098a05789b5f07e3
Helcio <helcioanicio19@gmail.com>
2023-03-18 23:28:28 -0300

    Added some elements on Portfolio to create the component

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,9 +1,100 @@
 import './Portfolio.modules.scss'
+import { FaReact, FaHtml5, FaSass } from 'react-icons/fa';
+import { TbBrandJavascript } from "react-icons/tb";
+import { IoLogoFirebase, IoLogoNodejs } from 'react-icons/io5'
+// import { IoLogoCss3 } from 'react-icons/io5'
+
+
 
 function Portfolio () {
     return (
         <>
-        <h6>Portfolio</h6>
+            <section className="SectionContainer">
+                <div className="DivContent">
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="DivImage">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="DivInformation">
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
         </>
     )
 }
