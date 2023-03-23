bba7efc727fd135f70af5606dfc955e71fb699e0
Helcio <helcioanicio19@gmail.com>
2023-03-23 15:32:55 -0300

    Update the logic of database and improve the form

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,34 +1,40 @@
 import './Footer.modules.scss';
+import { FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 // import {AiFillExclamationCircle} from 'react-icons/ai'
 
 import { useState } from 'react';
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore, addDoc } from "firebase/firestore";
+// import firebase from 'firebase/app';
+
 // import { getAnalytics } from "firebase/analytics";
 
-const firebaseApp = {
-    apiKey: "AIzaSyB8CF0qYveqNy67HZlHiM3C-vn6iWWMO6U",
-    authDomain: "portifolio-7ceba.firebaseapp.com",
-    projectId: "portifolio-7ceba",
-    storageBucket: "portifolio-7ceba.appspot.com",
-    messagingSenderId: "620380278249",
-    appId: "1:620380278249:web:20c93bbb7fd61189f10f78",
-    measurementId: "G-GNJGLKREKY"
-};
-const app = initializeApp(firebaseApp) ;
-// const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const userCollectionRef = collection(db, 'users');
-
-
-
-
 function Footer() {
+
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [telephone, setTelephone] = useState('')
     // const [proposal, setProposal] = useState('')
     const [formSubmitted, setFormSubmitted] = useState(false)
+
+
+    const firebaseApp = {
+        apiKey: "AIzaSyB8CF0qYveqNy67HZlHiM3C-vn6iWWMO6U",
+        authDomain: "portifolio-7ceba.firebaseapp.com",
+        projectId: "portifolio-7ceba",
+        storageBucket: "portifolio-7ceba.appspot.com",
+        messagingSenderId: "620380278249",
+        appId: "1:620380278249:web:20c93bbb7fd61189f10f78",
+        measurementId: "G-GNJGLKREKY"
+    };
+    // firebase.initializeApp(firebaseApp)
+
+    const app = initializeApp(firebaseApp);
+    // firebase.firestore().settings({ debug: true });
+
+    // const analytics = getAnalytics(app);
+    const db = getFirestore(app);
+    const userCollectionRef = collection(db, 'users');
 
     async function cadastrarUsuario(e) {
         e.preventDefault();
@@ -63,7 +69,7 @@ function Footer() {
                 <h3>Fale comigo!</h3>
                 <form onSubmit={cadastrarUsuario}>
                     <fieldset>
-                        <legend>Fomulário</legend>
+                        <legend>Formulário</legend>
 
                         <input  
                         type="text"
@@ -73,7 +79,7 @@ function Footer() {
                         value={name}
                         className={!name && formSubmitted ? 'messageError' : ''}
                         />
-                        {!name && formSubmitted && <div className='Error'><p>Please fill in the first name correctly</p></div>}
+                        {!name && formSubmitted && <div className='Error'><p>Por favor, coloque o nome corretamente.</p></div>}
 
                         <input  
                         type="email"
@@ -83,7 +89,7 @@ function Footer() {
                         value={email}
                         className={!email && formSubmitted ? 'messageError' : 'inputValid'}
                         />
-                        {formSubmitted && email === '' && <div className="Error"><p>Please fill in the email address correctly.</p></div>}
+                        {formSubmitted && email === '' && <div className="Error"><p>Por favor, coloque seu email corretamente.</p></div>}
                         
                         <input  
                         type="tel" 
@@ -94,7 +100,7 @@ function Footer() {
                         value={telephone}
                         className={!telephone && formSubmitted ? 'messageError' : 'inputValid'}
                         />
-                        {formSubmitted && telephone === '' && <div className="Error"><p>Please fill in the telephone correctly.</p></div>}
+                        {formSubmitted && telephone === '' && <div className="Error"><p>Por favor, coloque seu celular corretamente.</p></div>}
 
                         {/* <textarea 
                         name="Descrição" 
@@ -110,6 +116,17 @@ function Footer() {
 
                     </fieldset>
                 </form>
+                <div className='DivIcons'>
+                    <a href="https://wa.me/31991973835">
+                        <FaWhatsapp/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/helcio-anicio/">
+                        <FaLinkedin/>
+                    </a>
+                    <a href="https://www.instagram.com/helcioanicio/">
+                        <FaInstagram/>
+                    </a>
+                </div>
             </div>
         </>
     );
