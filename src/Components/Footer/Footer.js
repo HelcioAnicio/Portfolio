87b524d078d73b92a801da0d8d0f87c209107cb7
Helcio <helcioanicio19@gmail.com>
2023-05-24 22:36:18 -0300

    Retired form of footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,70 +1,67 @@
-import './Footer.modules.scss';
-import { FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import "./Footer.modules.scss";
+import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 // import {AiFillExclamationCircle} from 'react-icons/ai'
 
-
-import { useState } from 'react';
-import { initializeApp } from "firebase/app";
-import { collection, getFirestore, addDoc } from "firebase/firestore";
+// import { useState } from 'react';
+// import { initializeApp } from "firebase/app";
+// import { collection, getFirestore, addDoc } from "firebase/firestore";
 
 // import { getAnalytics } from "firebase/analytics";
 
 function Footer() {
+  //     const [name, setName] = useState('')
+  //     const [email, setEmail] = useState('')
+  //     const [telephone, setTelephone] = useState('')
+  //     const [proposal, setProposal] = useState('')
+  //     const [formSubmitted, setFormSubmitted] = useState(false)
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [telephone, setTelephone] = useState('')
-    const [proposal, setProposal] = useState('')
-    const [formSubmitted, setFormSubmitted] = useState(false)
+  //     const firebaseApp = {
+  //         apiKey: process.env.REACT_APP_API_KEY,
+  //         authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  //         projectId: process.env.REACT_APP_PROJECT_ID,
+  //         storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  //         messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  //         appId: process.env.REACT_APP_APP_ID,
+  //         measurementId: process.env.REACT_APPMEASUREMENT_ID,
+  //     };
 
-    const firebaseApp = {
-        apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_APP_ID,
-        measurementId: process.env.REACT_APPMEASUREMENT_ID, 
-    };
+  //     const app = initializeApp(firebaseApp);
+  //     // const analytics = getAnalytics(app);
+  //     const db = getFirestore(app);
+  //     const userCollectionRef = collection(db, 'users');
 
-    const app = initializeApp(firebaseApp);
-    // const analytics = getAnalytics(app);
-    const db = getFirestore(app);
-    const userCollectionRef = collection(db, 'users');
+  //     async function cadastrarUsuario(e) {
+  //         e.preventDefault();
+  //         if (name && email && telephone) {
+  //             console.log(`O Usuário foi cadastrado com os seguintes dados:
+  //                 Name:${name};
+  //                 Email:${email};
+  //                 telephone:${telephone}
+  //                 Proposal:${proposal}
+  //             `);
 
-    async function cadastrarUsuario(e) {
-        e.preventDefault();
-        if (name && email && telephone) {
-            console.log(`O Usuário foi cadastrado com os seguintes dados:
-                Name:${name};
-                Email:${email};
-                telephone:${telephone}
-                Proposal:${proposal}
-            `);
-    
-            try {
-                const user = await addDoc(userCollectionRef, {
-                    name,
-                    email,
-                    telephone,
-                    proposal,
-                });
+  //             try {
+  //                 const user = await addDoc(userCollectionRef, {
+  //                     name,
+  //                     email,
+  //                     telephone,
+  //                     proposal,
+  //                 });
 
-                console.log("Usuário cadastrado no Firebase:", user.id);
-            } catch (error) {
-                console.error("Erro ao cadastrar usuário no Firebase:", error);
-            }
-    
-        } else {
-            setFormSubmitted(true);
-        }
-    }
-    
-    
-    return (
-        <>
-            <section id='Footer' className="DivForm">
-                <div className="DivContainer">
+  //                 console.log("Usuário cadastrado no Firebase:", user.id);
+  //             } catch (error) {
+  //                 console.error("Erro ao cadastrar usuário no Firebase:", error);
+  //             }
+
+  //         } else {
+  //             setFormSubmitted(true);
+  //         }
+  //     }
+
+  return (
+    <>
+      <section id="Footer" className="DivForm">
+        {/* <div className="DivContainer">
                     <h3>Fale comigo!</h3>
                     <form onSubmit={cadastrarUsuario}>
                         <fieldset>
@@ -113,21 +110,21 @@ function Footer() {
                             <button type="submit" className='button'>Enviar formulario</button>
                         </fieldset>
                     </form> 
-                </div>
-                <div className='DivIcons'>
-                    <a href="https://wa.me/31991973835">
-                        <FaWhatsapp/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/helcio-anicio/">
-                        <FaLinkedin/>
-                    </a>
-                    <a href="https://www.instagram.com/helcioanicio/">
-                        <FaInstagram/>
-                    </a>
-                </div>
-            </section>
-        </>
-    );
+                </div> */}
+        <div className="DivIcons">
+          <a href="https://wa.me/31991973835">
+            <FaWhatsapp />
+          </a>
+          <a href="https://www.linkedin.com/in/helcio-anicio/">
+            <FaLinkedin />
+          </a>
+          <a href="https://www.instagram.com/helcioanicio/">
+            <FaInstagram />
+          </a>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Footer;
