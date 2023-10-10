e280cce3ceaeae7f1ab213531e8a5cd6b6ba191f
Helcio <helcioanicio19@gmail.com>
2023-10-09 21:33:52 -0300

    Update the field about techs

--- src/Components/Technology/Technology.js
+++ src/Components/Technology/Technology.js
@@ -2,6 +2,8 @@ import './Technology.modules.scss';
 import { FaReact, FaHtml5, FaSass } from 'react-icons/fa';
 import { TbBrandJavascript } from "react-icons/tb";
 import { IoLogoFirebase, IoLogoNodejs } from 'react-icons/io5'
+import { SiTailwindcss } from 'react-icons/si'
+
 // import { IoLogoCss3 } from 'react-icons/io5'
 
 
@@ -11,46 +13,60 @@ function Technology () {
             <section id='Tech' className='SectionTech'>
                 <h2>Tecnologias</h2>
                 <div className='DivContainer'>
+
                     <div className="React">
                         <div>
                             <FaReact/>
                         </div>
                         <p>React.Js</p>
                     </div>
+                    
                     <div className="Javascript">
                         <div>
                             <TbBrandJavascript/>
                         </div>
                         <p>JavaScript</p>
                     </div>
-                    <div className="Scss">
+                    
+                    <div className="Tailwind">
                         <div>
-                            <FaSass/>
+                            <SiTailwindcss/>
                         </div>
-                        <p>Scss</p>
+                        <p>Tailwind.Css</p>
                     </div>
-                    <div className="Html">
-                        <div>
-                            <FaHtml5/>
-                        </div>
-                        <p>Html5</p>
-                    </div>
-                    <div className="Node">
-                        <div>
-                            <IoLogoNodejs/>
-                        </div>
-                        <p>Node.Js</p>
-                    </div>
+
                     <div className="Firebase">
                         <div>
                             <IoLogoFirebase/>
                         </div>
                         <p>Firebase</p>
                     </div>
+                    
+                    <div className="Node">
+                        <div>
+                            <IoLogoNodejs/>
+                        </div>
+                        <p>Node.Js</p>
+                    </div>
+                    
+
+                    <div className="Scss">
+                        <div>
+                            <FaSass/>
+                        </div>
+                        <p>Sass</p>
+                    </div>
+                    
+                    <div className="Html">
+                        <div>
+                            <FaHtml5/>
+                        </div>
+                        <p>Html5</p>
+                    </div>
 
                 </div>
             </section>
-    </>
+        </>
     )
 }
 export default Technology;
