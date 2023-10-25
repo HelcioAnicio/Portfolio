5c9cb992e7d9483d03fad43a13cca6bd6c338739
Helcio <helcioanicio19@gmail.com>
2023-10-24 22:19:41 -0300

    Style: Merge componentes Home and About me in just one and styling the 'new' component

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,22 +1,32 @@
 import './AboutMe.modules.scss'
+// import '../Home/Home.modules.scss';
+
+import Photo  from '../../assets/profile.png';
+
 
 
 function AboutMe () {
     return (
         <>
-            <section id='AboutMe' className='ContainerAbout'>
-                <div className='DivText'>
-                    <h2 className='About'> Sobre mim</h2>
-                    <p className='Content'>
-                        Olá! Meu nome é Helcio, apaixonado por criar experiências digitais incríveis. 
-                        Acredito que, para criar interfaces de usuário incríveis, é necessário estar <span>sempre à frente do jogo</span>. 
-                        É por isso que <span>me dedico a criar interfaces intuitivas e fáceis de usar</span>, que possam ser facilmente <span>adaptadas às necessidades específicas de cada cliente</span>.
-                        <br /><br />
-                        Se você estiver procurando por um desenvolvedor front-end altamente qualificado e motivado, <span>não hesite em entrar em contato comigo!</span> Estou animado para discutir como posso ajudar a sua empresa a ter sucesso no mundo digital.
+
+            <section className='sectionContainer'>
+                <h2>SOBRE MIM</h2>
+                <div className='divFigure'>
+                    <figure className='figure'>
+                        <img src={Photo} alt="profile"/> 
+                    </figure>
+                </div>
+                <div className="divText">
+                    <h4>Olá! Meu nome é Helcio,</h4>
+                    <p>apaixonado por criar experiências digitais incríveis. Acredito que, para criar interfaces de usuário, é necessário estar sempre à frente do jogo. É por isso que me dedico a criar interfaces intuitivas e fáceis de usar, que possam ser facilmente adaptadas às necessidades específicas de cada cliente.
+                        Se você estiver procurando por um desenvolvedor para o seu negócio altamente qualificado e motivado, não hesite em entrar em contato comigo! Estou animado para discutir como ajudar a sua empresa a ter sucesso no mundo digital.
                     </p>
                 </div>
             </section>
-           
+
+
+            <section id='Home' className="">
+            </section>
         </>
     )
 }
