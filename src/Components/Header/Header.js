5120c49f95ba826924b5d2eca205da9e1e89699e
Helcio <helcioanicio19@gmail.com>
2023-12-02 22:37:13 -0300

    STYLE: Improve css to Screen's mobile

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,8 @@ import './Header.modules.scss';
 // import { GiHamburgerMenu } from "react-icons/gi";
 // import { TfiClose } from "react-icons/tfi";
 // import { useState, useEffect} from 'react';
+// import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;500&display=swap');
+// import 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;500&display=swap'
 
 function Header () {
 	// const [isOpen, setIsOpen] = useState(false)
