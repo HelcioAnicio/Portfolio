37348d749714b50dc69ff8b5684c7b0caf6fc6ad
Helcio Anicio <helcioanicio19@gmail.com>
2025-01-13 10:45:49 -0300

    Feat: Created Repository component  - Show total repositories

---
+++ src/components/ui/repository.tsx
@@ -0,0 +1,11 @@
+import { Card } from './card';
+
+export const GitHubRepos = () => {
+  return (
+    <Card className='max-w-44 p-2 text-lg '>
+      Tenho{' '}
+      <span className='font-serif text-primary-foreground'>32 projetos</span>{' '}
+      desenvolvidos.
+    </Card>
+  );
+};
