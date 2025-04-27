5ff20dbf9af7dec6be951dc7484b904d4cd47976
Helcio Anicio <helcioanicio19@gmail.com>
2025-04-26 21:44:51 -0300

    Feat: Improved style from list project

--- src/components/ui/listProject.tsx
+++ src/components/ui/listProject.tsx
@@ -51,147 +51,146 @@ import Autoplay from "embla-carousel-autoplay";
 
 export const ListProjects = () => {
   return (
-    <>
-      <Card className="bg-mome mx-auto scroll-smooth">
-        <CardHeader>
-          <CardTitle className="flex w-full items-center gap-1 py-5 text-2xl text-primary-foreground sm:py-10">
-            <FaRegWindowRestore />
-            <p className="min-w-max"> Pricipais Projetos</p>
-          </CardTitle>
-          {/* <GitHubRepos /> */}
-        </CardHeader>
-        <CardContent className="p-1 pb-5">
-          <Carousel
-            plugins={[
-              Autoplay({
-                delay: 5000,
-              }),
-            ]}
-            opts={{
-              align: "start",
-              loop: true,
-            }}
-          >
-            <CarouselContent>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopAgroFinance}
-                  img={MobileAgroFinance}
-                  vercel={"https://agro-finance-real.vercel.app/"}
-                  github={"https://github.com/HelcioAnicio/AgroFinance"}
-                  nameProject="Em construção"
-                  descriptionProject="Esse é sistema SAAS chamado inicialmente AgroFinance, objetivo ter total controle do animais da fazenda. Dentro desse sistema é possivel gerenciar animais, e o financeiro da fazendo tudo de forma automatizada."
-                  techIcons={[
-                    TbBrandNextjs,
-                    FaReact,
-                    SiTypescript,
-                    SiPrisma,
-                    SiSupabase,
-                    SiPostgresql,
-                    SiTailwindcss,
-                    SiShadcnui,
-                    BsStripe,
-                  ]}
-                />
-              </CarouselItem>
+    <Card className="bg-nome mx-auto h-dvh w-full max-w-5xl scroll-smooth border-none">
+      <CardHeader>
+        <CardTitle className="flex w-full items-center gap-1 py-5 text-2xl text-primary-foreground sm:py-10">
+          <FaRegWindowRestore className="text-foreground" />
+          <p className="min-w-max text-foreground"> Pricipais Projetos</p>
+        </CardTitle>
+        {/* <GitHubRepos /> */}
+      </CardHeader>
+      <CardContent className="p-1 pb-5">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 5000,
+            }),
+          ]}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopAgroFinance}
+                img={MobileAgroFinance}
+                vercel={"https://agro-finance-real.vercel.app/"}
+                github={"https://github.com/HelcioAnicio/AgroFinance"}
+                nameProject="Em construção"
+                descriptionProject="Esse é sistema SAAS chamado inicialmente AgroFinance, objetivo ter total controle do animais da fazenda. Dentro desse sistema é possivel gerenciar animais, e o financeiro da fazendo tudo de forma automatizada."
+                techIcons={[
+                  TbBrandNextjs,
+                  FaReact,
+                  SiTypescript,
+                  SiPrisma,
+                  SiSupabase,
+                  SiPostgresql,
+                  SiTailwindcss,
+                  SiShadcnui,
+                  BsStripe,
+                ]}
+              />
+            </CarouselItem>
 
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopEcommerce}
-                  img={MobileEcommerce}
-                  vercel={
-                    "https://ecommerce-fsw-git-main-helcioanicios-projects.vercel.app/"
-                  }
-                  github={"https://github.com/HelcioAnicio/Ecommerce-FSW"}
-                  nameProject="Ecommerce FSW"
-                  descriptionProject="O site oferece login simplificado com contas do Google, navegação por categorias de produtos e opções de descontos. Os usuários podem gerenciar seu carrinho de compras, incluindo adicionar, remover e modificar itens. O pagamento é seguro e eficiente através da integração com a API do Stripe, com suporte a webhooks para processar eventos de pagamento."
-                  techIcons={[
-                    FaReact,
-                    SiTypescript,
-                    SiPrisma,
-                    TbBrandNextjs,
-                    SiSupabase,
-                    SiTailwindcss,
-                    BsStripe,
-                  ]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopUrl}
-                  img={MobileUrl}
-                  vercel={"https://url-shortener-seven-delta.vercel.app/"}
-                  github={"https://github.com/HelcioAnicio/url-shortener"}
-                  nameProject="Encurtador de Links"
-                  descriptionProject="O Encurtador de URL é uma ferramenta grátis para encurtar links
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopEcommerce}
+                img={MobileEcommerce}
+                vercel={
+                  "https://ecommerce-fsw-git-main-helcioanicios-projects.vercel.app/"
+                }
+                github={"https://github.com/HelcioAnicio/Ecommerce-FSW"}
+                nameProject="Ecommerce FSW"
+                descriptionProject="O site oferece login simplificado com contas do Google, navegação por categorias de produtos e opções de descontos. Os usuários podem gerenciar seu carrinho de compras, incluindo adicionar, remover e modificar itens. O pagamento é seguro e eficiente através da integração com a API do Stripe, com suporte a webhooks para processar eventos de pagamento."
+                techIcons={[
+                  FaReact,
+                  SiTypescript,
+                  SiPrisma,
+                  TbBrandNextjs,
+                  SiSupabase,
+                  SiTailwindcss,
+                  BsStripe,
+                ]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopUrl}
+                img={MobileUrl}
+                vercel={"https://url-shortener-seven-delta.vercel.app/"}
+                github={"https://github.com/HelcioAnicio/url-shortener"}
+                nameProject="Encurtador de Links"
+                descriptionProject="O Encurtador de URL é uma ferramenta grátis para encurtar links
                 de redes sociais do Instagram, Facebook, YouTube, Twitter,
                 Linked In, Pinterest e sites com autoridade na Internet."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopBarberDev}
-                  img={MobileBarberDev}
-                  vercel={
-                    "https://dev-barber-ex-helcioanicios-projects.vercel.app/"
-                  }
-                  github={"https://github.com/HelcioAnicio/devBarberEx"}
-                  nameProject="Barber Dev"
-                  descriptionProject="Este projeto é um website para uma barbearia, mostrando o designer e também um campo para agendamento como exemplo."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopFernandoSantana}
-                  img={MobileFernandoSantana}
-                  vercel={""}
-                  github={
-                    "https://github.com/HelcioAnicio/website-FernandoSantanaLawyers"
-                  }
-                  nameProject="Fernando Advogados"
-                  descriptionProject="Um grupo de advocacia, mostrando um designer intuitivo, objetivo. Foco na demonstração dos serviços e também de conquistas da empresa."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopGfAdvogados}
-                  img={MobileGfAdvogados}
-                  vercel={
-                    "https://gf-advogados-helcioanicios-projects.vercel.app/"
-                  }
-                  github={"https://github.com/HelcioAnicio/MarketingDigital"}
-                  nameProject="Marketing Digital"
-                  descriptionProject="Empresa consolidada, de um advocacia, mostrando a equipe e todos os serviços prestados no meio juridico, e claro mostrando os feedbacks e clientes atendidos."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopBarberDev}
+                img={MobileBarberDev}
+                vercel={
+                  "https://dev-barber-ex-helcioanicios-projects.vercel.app/"
+                }
+                github={"https://github.com/HelcioAnicio/devBarberEx"}
+                nameProject="Barber Dev"
+                descriptionProject="Este projeto é um website para uma barbearia, mostrando o designer e também um campo para agendamento como exemplo."
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopFernandoSantana}
+                img={MobileFernandoSantana}
+                vercel={""}
+                github={
+                  "https://github.com/HelcioAnicio/website-FernandoSantanaLawyers"
+                }
+                nameProject="Fernando Advogados"
+                descriptionProject="Um grupo de advocacia, mostrando um designer intuitivo, objetivo. Foco na demonstração dos serviços e também de conquistas da empresa."
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopGfAdvogados}
+                img={MobileGfAdvogados}
+                vercel={
+                  "https://gf-advogados-helcioanicios-projects.vercel.app/"
+                }
+                github={"https://github.com/HelcioAnicio/MarketingDigital"}
+                nameProject="Marketing Digital"
+                descriptionProject="Empresa consolidada, de um advocacia, mostrando a equipe e todos os serviços prestados no meio juridico, e claro mostrando os feedbacks e clientes atendidos."
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
 
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopMarketing}
-                  img={MobileMarketingDigital}
-                  vercel={"https://marketing-digital-eight.vercel.app/"}
-                  github={"https://github.com/HelcioAnicio/MarketingDigital"}
-                  nameProject="Marketing Digital"
-                  descriptionProject="Empresa de marketing digital, mostra os serviços, cards de orçamento e redes sociais, além de conter um form ao final para realizar contato."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopMonguilho}
-                  img={MobileMonguilhoDental}
-                  vercel={"https://monguilho-dental.vercel.app/"}
-                  github={"https://github.com/HelcioAnicio/MonguilhoDental"}
-                  nameProject="Monguilho Dental - Clinica"
-                  descriptionProject="Esse site mostra uma clinica dentária, mostrando os serviços, dados e informações da clinica."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              {/* <CarouselItem className='lg:basis-1/3'>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopMarketing}
+                img={MobileMarketingDigital}
+                vercel={"https://marketing-digital-eight.vercel.app/"}
+                github={"https://github.com/HelcioAnicio/MarketingDigital"}
+                nameProject="Marketing Digital"
+                descriptionProject="Empresa de marketing digital, mostra os serviços, cards de orçamento e redes sociais, além de conter um form ao final para realizar contato."
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopMonguilho}
+                img={MobileMonguilhoDental}
+                vercel={"https://monguilho-dental.vercel.app/"}
+                github={"https://github.com/HelcioAnicio/MonguilhoDental"}
+                nameProject="Monguilho Dental - Clinica"
+                descriptionProject="Esse site mostra uma clinica dentária, mostrando os serviços, dados e informações da clinica."
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            {/* <CarouselItem className='lg:basis-1/3'>
                 <Project
                   backgroundUrl={DesktopWebSite}
                   img={MobileWebSiteDigital}
@@ -202,51 +201,50 @@ export const ListProjects = () => {
                   techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
                 />
               </CarouselItem> */}
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopCarSeller}
-                  img={MobileCarSeller}
-                  vercel={"https://car-seller-eta.vercel.app/"}
-                  github={"https://github.com/HelcioAnicio/Car-seller"}
-                  nameProject="Vendas de veiculos"
-                  descriptionProject="Aqui você conseguirá ver uma concessionária de carros, com preços, fotos, preços e outras coisas sobre os carros."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <Project
-                  backgroundUrl={DesktopFaq}
-                  img={MobileFAQ}
-                  vercel={"https://faq-questions.vercel.app/"}
-                  github={"https://github.com/HelcioAnicio/FAQ_Questions"}
-                  nameProject="FAQ (Perguntas frequentes)"
-                  descriptionProject="Frequently Asked Questions é um site onde você vai encontrar
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopCarSeller}
+                img={MobileCarSeller}
+                vercel={"https://car-seller-eta.vercel.app/"}
+                github={"https://github.com/HelcioAnicio/Car-seller"}
+                nameProject="Vendas de veiculos"
+                descriptionProject="Aqui você conseguirá ver uma concessionária de carros, com preços, fotos, preços e outras coisas sobre os carros."
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <Project
+                backgroundUrl={DesktopFaq}
+                img={MobileFAQ}
+                vercel={"https://faq-questions.vercel.app/"}
+                github={"https://github.com/HelcioAnicio/FAQ_Questions"}
+                nameProject="FAQ (Perguntas frequentes)"
+                descriptionProject="Frequently Asked Questions é um site onde você vai encontrar
             algumas perguntas e respostas mais frequentes realizadas sobre
             mim."
-                  techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
-                />
-              </CarouselItem>
-              <CarouselItem className="lg:basis-1/3">
-                <div className="flex w-full max-w-lg flex-col items-center p-4">
-                  <div className="flex h-60 w-full animate-spin items-center justify-center">
-                    <AiOutlineLoading3Quarters size={64} />
-                  </div>
-                  <div className="text-center">
-                    <h3 className="mt-4 text-lg">Em breve</h3>
-                    <div className="mt-4 flex gap-2">
-                      <AiOutlineQuestionCircle size="24" />
-                      <AiOutlineQuestionCircle size="24" />
-                      <AiOutlineQuestionCircle size="24" />
-                    </div>
+                techIcons={[TbBrandJavascript, FaHtml5, IoLogoCss3]}
+              />
+            </CarouselItem>
+            <CarouselItem className="lg:basis-1/3">
+              <div className="flex w-full max-w-lg flex-col items-center p-4">
+                <div className="flex h-60 w-full animate-spin items-center justify-center">
+                  <AiOutlineLoading3Quarters size={64} />
+                </div>
+                <div className="text-center">
+                  <h3 className="mt-4 text-lg">Em breve</h3>
+                  <div className="mt-4 flex gap-2">
+                    <AiOutlineQuestionCircle size="24" />
+                    <AiOutlineQuestionCircle size="24" />
+                    <AiOutlineQuestionCircle size="24" />
                   </div>
                 </div>
-              </CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious className="" />
-            <CarouselNext className="" />
-          </Carousel>
-        </CardContent>
-      </Card>
-    </>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="" />
+          <CarouselNext className="" />
+        </Carousel>
+      </CardContent>
+    </Card>
   );
 };
--- src/components/ui/project.tsx
+++ src/components/ui/project.tsx
@@ -1,4 +1,4 @@
-import { IconType } from 'react-icons';
+import { IconType } from "react-icons";
 // import {
 //   HoverCard,
 //   HoverCardContent,
@@ -7,16 +7,15 @@ import { IconType } from 'react-icons';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
+} from "@/components/ui/dialog";
 
-import Image, { StaticImageData } from 'next/image';
-import { Button } from './button';
-import Link from 'next/link';
-import { Github, Globe } from 'lucide-react';
+import Image, { StaticImageData } from "next/image";
+import { Button } from "./button";
+import Link from "next/link";
+import { Github, Globe } from "lucide-react";
 
 interface Project {
   style?: string; // Mantido caso você queira passar estilos adicionais
@@ -30,7 +29,7 @@ interface Project {
 }
 
 export const Project: React.FC<Project> = ({
-  style = '',
+  style = "",
   img,
   vercel,
   github,
@@ -41,81 +40,80 @@ export const Project: React.FC<Project> = ({
 }) => {
   return (
     <Dialog>
-      <div className={`flex flex-col m-auto min-w-48 max-w-sm ${style} w-full`}>
+      <div
+        className={`m-auto flex min-w-48 max-w-sm flex-col gap-6 ${style} w-full rounded-lg border border-primary-foreground p-7`}
+      >
         <div
           style={{
             backgroundImage: `url(${backgroundUrl.src})`,
-            backgroundPosition: 'center center',
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat',
+            backgroundPosition: "center center",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
           }}
-          className={`w-full max-w-96 h-48 m-auto relative overflow-hidden rounded-lg`}>
+          className={`relative m-auto h-48 w-full max-w-96 overflow-hidden rounded-lg`}
+        >
           <Image
             src={img}
-            alt='Mobile'
-            className='w-12 h-24 absolute bottom-1 right-1 sm:w-16 sm:h-32'
+            alt="Mobile"
+            className="absolute bottom-1 right-1 h-24 w-12 sm:h-32 sm:w-16"
           />
         </div>
-        <div className='flex flex-col mt-4 px-1'>
+        <div className="mt-4 flex flex-col gap-2 px-1">
           <h3
-            className='text-xl font-bold'
-            title={nameProject}>
+            className="text-3xl font-bold text-primary-foreground"
+            title={nameProject}
+          >
             {nameProject}
           </h3>
           <p
-            className=' line-clamp-4 overflow-hidden text-ellipsis '
-            title='Description of Project'>
+            className="line-clamp-4 overflow-hidden text-ellipsis"
+            title="Description of Project"
+          >
             {descriptionProject}
           </p>
         </div>
-        <DialogTrigger className='mt-4 mr-auto px-3 py-2 text-primary-foreground bg-primary rounded-md'>
+        <DialogTrigger className="mr-auto mt-4 rounded-md bg-primary px-3 py-2 text-primary-foreground hover:bg-secondary">
           Mais detalhes
         </DialogTrigger>
-        <DialogContent>
+        <DialogContent className="flex h-1/2 flex-col items-start justify-evenly gap-10">
           <DialogHeader>
-            <DialogTitle className='text-xl font-bold'>
+            <DialogTitle className="text-3xl font-bold text-primary-foreground">
               {nameProject}
             </DialogTitle>
-            <DialogDescription>
-              <p className=''>{descriptionProject}</p>
-              <div className='flex mt-2 gap-3'>
-                {techIcons.map((Icon, index) => (
-                  <Icon
-                    key={index}
-                    title={`${Icon.name}= Tecnologie's name`}
-                    className='text-lg'
-                  />
-                ))}
-              </div>
-              <div className='flex mt-4'>
-                {vercel !== '' && (
-                  <>
-                    <Link
-                      href={vercel}
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      className='mr-4'>
-                      <Button
-                        title='Ver site'
-                        className='hover:bg-card'>
-                        Ver Site <Globe />
-                      </Button>
-                    </Link>
-                  </>
-                )}
-                <Link
-                  href={github}
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                  <Button
-                    title='Ver código fonte'
-                    className='hover:bg-card'>
-                    Ver Código <Github />
-                  </Button>
-                </Link>
-              </div>
-            </DialogDescription>
           </DialogHeader>
+          <p className="text-xl">{descriptionProject}</p>
+          <div>
+            <div className="mt-2 flex gap-3">
+              {techIcons.map((Icon, index) => (
+                <Icon
+                  key={index}
+                  title={`${Icon.name}= Tecnologie's name`}
+                  className="text-lg"
+                />
+              ))}
+            </div>
+            <div className="mt-4 flex">
+              {vercel !== "" && (
+                <>
+                  <Link
+                    href={vercel}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-4"
+                  >
+                    <Button title="Ver site" className="hover:bg-card">
+                      Ver Site <Globe />
+                    </Button>
+                  </Link>
+                </>
+              )}
+              <Link href={github} target="_blank" rel="noopener noreferrer">
+                <Button title="Ver código fonte" className="hover:bg-card">
+                  Ver Código <Github />
+                </Button>
+              </Link>
+            </div>
+          </div>
         </DialogContent>
       </div>
     </Dialog>
